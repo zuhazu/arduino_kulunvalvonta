b52f08d96954a2b9b38132a25eef39f59e26639c
zuhazu <116634372+zuhazu@users.noreply.github.com>
2024-11-25 15:08:24 +0200

    Koodi kommentoitu ja viimeistelyä vaille valmis.

--- kulunvalvonta/kulunvalvonta.ino
+++ kulunvalvonta/kulunvalvonta.ino
@@ -1,149 +1,165 @@
-#include <base64.hpp>
+/**********************************************************************************************************************
 
-//#include <arduino_base64.hpp>
-//#include <b64.h>
+Tässä ohjelmassa käytetään Arduino UNO WiFi Rev 2 controlleria, Grove shieldiä, RFID-RC522 lukijaa, Grove LCD- näyttöä ja 1PC passive buzzeria.
 
-#include <HttpClient.h>
-#include <SPI.h>
-#include <MFRC522.h>
-#include <WiFi.h>
-//Näytön kirjastot
-#include <Wire.h>
-#include "rgb_lcd.h"
+Ohjelman tarkoituksena on yhdistää Wifin kautta toisaalla olevaan serveriin, löytää yhteys, jolloin RFID tagia näyttämällä lähetetään tieto serverille.
+Serverillä tieto käsitellään ja palautetaan tieto Arduinolle. Riippuen palautuksesta annetaan näyttöön ja Buzzeriin vastaus.
 
-rgb_lcd lcd;
+**********************************************************************************************************************/
 
+#include <base64.hpp> // Base koodauksen tuki.
+#include <HttpClient.h> // HTTP- pyyntöjen käsittelyyn.
+#include <SPI.h> // Otetaan käyttöön SPI- väylät RFID-RC522 - lukijaa varten. 
+#include <MFRC522.h> // RFID lukijan tuki.
+#include <WiFi.h> // Kirjasto WiFi yhteyksien hallintaan.
+#include <Wire.h> // I2C väylän tuki näyttöä varten.
+#include "rgb_lcd.h" // Itse bäytön tuki.
+
+rgb_lcd lcd; // Alustetaan näyttö tuki.
+
+// Määritetään näytön väri punaiseksi alussa.
 const int colorR = 255;
 const int colorG = 0;
 const int colorB = 0;
 
-#define RST_PIN         9          // Configurable, see typical pin layout above
-#define SS_PIN          10         // Configurable, see typical pin layout above
+//Määritetään RFID- lukijan pinnit
+#define RST_PIN         9          // Reset pinni (RFID- Lukija)
+#define SS_PIN          10         // Chip select (RFID- Lukija)
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
-WiFiClient client;
+int buzzerPin = 13; //Buzzerin määrittäminen pininlle 13
 
-//ssid:036-Wifi
-//pass:036luokka
+// Luodaan MFRC522-instanssi RFID-lukijaa varten
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // MFRC522-instanssi, käytetään pinnejä 10 (SS) ja 9 (RST)
+WiFiClient client; 
 
+// Muistiinpano 036-luokan tunnuksia varten.
+// ssid:036-Wifi
+// pass:036luokka
+
+// Wifi yhteyden tiedot.
 char ssid[] = "036-Wifi";
 char pass[] = "036luokka";
 int keyIndex = 0;
-int status = WL_IDLE_STATUS;
-//192.168.43.47
+int status = WL_IDLE_STATUS; // Wi-Fi-yhteyden tila, aluksi lepotilassa
+
+// Määritellään palvelimen IP-osoite (asetetaan riippuen kuka sen nostaa pystyyn.)
 IPAddress server(192,168,1,105);
 
 void setup() {
-	Serial.begin(9600);		// Initialize serial communications with the PC
-	while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
-	SPI.begin();			// Init SPI bus
+	Serial.begin(9600);		// Alustetaan sarjaportti tietokoneelle
+	while (!Serial);		// Odotetaan että portti on valmis (added for Arduinos based on ATMEGA32U4)
+	SPI.begin();			// Alustetaan SPI-väylä (tarvitaan MFRC522:n kanssa)
   initLCD();    //Näytön käyttöönotto
+  pinMode(buzzerPin, OUTPUT);     //Määritetään buzzerin pinni ulostuloksi.
 
-  initRFID();
-  initWifi();
+  initRFID();   //RFID lukijan käyttöönotto
+  initWifi();   //WiFi toiminnon käyttöönotto
   delay(1000);
 
 }
-
+// Käynnistäessä Asetetaan näyttöön teksti.
 void initLCD()
 {
   lcd.begin(16, 2);
   lcd.setRGB(colorR, colorG, colorB);
-  lcd.print("Jenninator");
+  lcd.print("Easy Access");
 }
-
+// Alustetaan RFID- lukija
 void initRFID()
 {
   mfrc522.PCD_Init();
-  mfrc522.PCD_DumpVersionToSerial();
+  mfrc522.PCD_DumpVersionToSerial(); // Tulostetaan tiedot sarjaporttiin.
 }
 
+// Alustetaan WiFi- yhteys
 void initWifi()
 {
-  if (WiFi.status() == WL_NO_SHIELD) 
+  if (WiFi.status() == WL_NO_SHIELD) // Jos shield ei ole...
   {
-    Serial.println("WiFi kuori ei ole paikallaan");
-    while(true);
+    Serial.println("WiFi kuori ei ole paikallaan"); // ...Tulosteteen virhe.
+    while(true); 
   }
-
+  // Yritetään muodostaa yhteys WiFi verkkoon. 
   while(status != WL_CONNECTED){
-    lcd.setCursor(0, 1);
-    lcd.print("Connecting");
-    Serial.print("Yritethään yhistää SSID: ");
-    Serial.println(ssid);
-    status = WiFi.begin(ssid, pass);
+    lcd.setCursor(0, 1); // Asetetaan kursori toiselle riville...
+    lcd.print("Connecting"); // ...Jolle printataan tämä.
+    Serial.print("Yritethään yhistää SSID: "); // Serialiin pritntataan tietoisesti kirjoitusvirheellä..
+    Serial.println(ssid); // ... WiFi:n SSID 
+    status = WiFi.begin(ssid, pass); // Yritetään yhdistää WiFi- verkkoon.
     delay(5000);
   }
-  //tulisiko lisätä timeout?
-  lcd.clear();
-  lcd.setRGB(0, 255, 0);
-  lcd.setCursor(0, 0);
-  lcd.print("Connected");
-  Serial.println("Yhristetty Wifiin");
+  
+  // Kun on yhdistetty:
+  lcd.clear(); // Tyhjätään näyttö.
+  lcd.setRGB(0, 255, 0); // Asetetaan näytön väri vihreäksi.
+  lcd.setCursor(0, 0); // Kursori ekalle riville.
+  lcd.print("Connected"); // Printataan näyttöön tämä.
+  Serial.println("Yhristetty Wifiin"); // Printataan sarjaporttiin tämä.
 }
-
+/*********************************************************************************************************************************
+Tässä osassa tarkastetaan kortin läsnäolo, luodaan HTTP PUT-pyynö serverille, otetaan autentikointi käyttöön, otetaan vastaus serveriltä,
+parsitaan vastaus ja siitä riippuen palautetaan näyttöön tietty arvo.
+**********************************************************************************************************************************/
 void loop() {
-	// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+	// Tarkastetaan onko kortti läsnä. Jos ei, palataan odottamaan korttia
 	if ( ! mfrc522.PICC_IsNewCardPresent()) {
 		return;
 	}
-
-	// Select one of the cards
+	// Valitaan kortti ja luetaan sen sarjanumero
 	if ( ! mfrc522.PICC_ReadCardSerial()) {
 		return;
 	}
-String card_id = "";
+String card_id = ""; // Luodaan muuttuja kortin UID:tä varten.
 
-  // Loop through the UID bytes and append them to the string
+  // Käydään UID: tavut ja lisätään ne merkkidataan hexadesimaaleissa.
   for (byte i = 0; i < mfrc522.uid.size; i++) {
-    card_id += String(mfrc522.uid.uidByte[i], HEX);  // Convert each byte to hexadecimal
+    card_id += String(mfrc522.uid.uidByte[i], HEX);  // Muutetaan tavut hexadesimaaleiksi
   }
-
-  // Optionally, make the string uppercase for consistency
-  card_id.toUpperCase();
-  // Print the UID for debugging
+  card_id.toUpperCase(); // Muutetaan isoiksi kirjaimiksi, jotta olisi yhdenmukainen.
   Serial.print("Card UID: ");
-  Serial.println(card_id);
-  //LCD
-  lcd.clear();
+  Serial.println(card_id); // Printataan kortin ID debuggausta varten.
+  lcd.clear(); // Pyyhitään näyttö.
+    // Määritetään Käyttäjätunnus ja Salasana.
     String username = "admin";
     String password = "password";
-    String request = "PUT /tag HTTP/1.1\n";
-    request = request + "host: 192.168.1.105:8080\n";
-    request = request + "content-type: application/json\n";
-    request = request + "Connection: keep-alive\n";
 
-    String body = "{\"tag_id\":\"" + card_id + "\", \"room_id\":\"036\"}\n";  // JSON-rakenne, jossa on card_id
-    request = request + "Content-Length: " + String(body.length()) + "\n";  // Lisätään body-pituus
-    //request = "\n";  // Tyhjä rivi otsikoiden ja bodyn väliin
+    String request = "PUT /api/tag HTTP/1.1\n"; // Muodostetaan HTTP pyyntö PUT menetelmällä + rivinvaihto
+    request = request + "host: 192.168.1.105:8080\n"; // Määritetään palvelimen osoite + rivinvaihto
+    request = request + "content-type: application/json\n"; // Määritellään sisältötyyppi JSON + rivinvaihto
+    request = request + "Connection: keep-alive\n"; // Yhteys pidetään elossa + rivinvaihto
+
+    // Luodaan JSON-muotoinen body, joka sisältää kortin UID:n ja huoneen tunnuksen
+    String body = "{\"tag_id\":\"" + card_id + "\", \"room_id\":\"100100\"}\n";  // JSON-rakenne, jossa on card_id
+    request = request + "Content-Length: " + String(body.length()) + "\n";  // Lisätään body-pituus HTTP otsikkoon
+
+    // Luodaan perusutentikointi käyttäjätunnus ja salasana.
     char auth[] = "admin:password";  // Muodostetaan 'username:password' yhdistelmä
-    char encoded[32];
-    //encode_base64 (auth,12,encoded);
-    encode_base64((unsigned char *) auth, 14, encoded);
-    String authAsString = String(encoded);
-    Serial.println("Enkootattu : : : " + authAsString);
+    char encoded[32]; // Taulukko, johon Base64-koodattu autentikointi tallennetaan.
 
-    //String temp = "Basic YWRtaW46cGFzc3dvcmQ=";
-    
-    request = request + "Authorization: Basic " + authAsString;  // Lisätään Basic Auth -otsikko
+    encode_base64((unsigned char *) auth, 14, encoded); // Base64-koodaus 'username:password' -yhdistelmästä.
+    String authAsString = String(encoded); // Muutetaan Base64-koodattu autentikointi merkkijonoksi.
+    Serial.println("Enkootattu : : : " + authAsString); // Tulostetaan Base64-koodattu autentikointi debug-tarkoituksiin
+
+    // Lisätään 'Authorization' -otsikko HTTP-pyyntöön (Basic Authentication).
+    request = request + "Authorization: Basic " + authAsString; 
     request = request + "\n\n";
     request = request + body;
   
+  // Yritetään muodostaa yhteys palvelimelle
   if (client.connect(server, 8080)) {
-
     Serial.print("connected to server: ");
-    Serial.println(server);
+    Serial.println(server); // Printataan serveri johon ollaan yhdistetty.
+    Serial.println(request); // Tulostetaan pyyntö palvelimen tarkistamista varten.
+    client.print(request); // Lähetetään pyyntö palvelimelle
+    client.println(); // Pyynnön jälkeen yhjä rivi. 
 
-    Serial.println(request);
-
-    client.print(request);
-
-    client.println();
+    bool isBody = false; // Muuttuja, joka seuraa onko palvelimelta tullut body
+    String body = ""; // Muuttuja palvelimen vastauksen bodyn tallentamiseen
 
 
-    bool isBody = false;
-    String body = "";
-
+/**************************************************************************
+Tässä parsitaan body, jotta saadaan yksi rivinen vastaus LCD näyttöä varten
+***************************************************************************/
     while (client.connected() || client.available()) {
       String line = client.readStringUntil('\n'); // Luetaan rivi riviltä
 
@@ -152,46 +168,35 @@ String card_id = "";
         isBody = true; // Body alkaa tästä
         continue;
       }
-
       if (isBody) {
         body += line; // Lisätään bodyyn
-        break;
+        break; // Lopetetaan luenta ensimmäisen rivin jälkeen.
       }
     }
-
-    // Tulostetaan pelkkä body
+    // Tulostetaan pelkkä body (vastaus palvelimelta)
     Serial.println("Response Body:");
     Serial.println(body);
-    // Tulostetaan palvelimen vastaus
- //   Serial.println("Server response: " + response);
-updateScreenColor(body);
-    //Serial.println(body);
-    //Serial.println(response);
+
+    updateScreenColor(body); // Päivitetään näyttö palvelimen vastauksen perusteella.
     delay(1000);
-
-
   }
-	// Dump debug info about the card; PICC_HaltA() is automatically called
-	//mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-  wifiLoop();
+wifiLoop(); //Kutsutaan alla olevaa wifi silmukkaa.
 }
-
 void wifiLoop() {
+  // Luetaan vastaanotettu data Wi-Fi-yhteydeltä, jos sitä on saatavilla.
   while (client.available()) {
-
-    char c = client.read();
-
-    Serial.write(c);
-
+    char c = client.read(); // Lue merkki.
+    Serial.write(c); // Tulosta merkki sarjaporttiin.
   }
+  // Jos yhteys on katkennut, suljetaan yhteys
   if (!client.connected()) {
-
-    Serial.println();
-    client.stop();
-
+    Serial.println(); // Tyhjä rivi
+    client.stop(); // Suljetaan yhteys
   }
 }
-
+/******************************************************************************************************************************
+Tämä pätkä ottaa binääri dataa ja muuttaa sen Base64 muotoon. Auttaa datan lähetykseen verkon yli.
+*******************************************************************************************************************************/
 const char base64_table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 void encode_base64_r(const unsigned char *input, size_t length, char *encoded) {
@@ -229,6 +234,7 @@ void encode_base64_r(const unsigned char *input, size_t length, char *encoded) {
     encoded[j] = '\0';
 }
 
+//Tässä kohdassa Asetetaan näytön tila ja annetaan ääni riippuen onko Body "logged in", "logged out" taikka "Access denied"
 void updateScreenColor(String body) {
   lcd.clear();  // Tyhjennetään näyttö ennen väriä ja tekstiä
 
@@ -237,16 +243,23 @@ void updateScreenColor(String body) {
     lcd.setRGB(0, 255, 0);  // Vihreä väri (Logged in)
     lcd.setCursor(0, 0);
     lcd.print("Logged in");
+    tone(buzzerPin, 1200, 200);  // 1200 Hz taajuus 200 millisekunnin ajan
   } 
-  else if (body == "Access denied") {
+  else if (body == "Access denied" || "Room not found") {
     lcd.setRGB(255, 0, 0);  // Punainen väri (Access denied)
     lcd.setCursor(0, 0);
-    lcd.print("Access denied");
+    lcd.print(body);
+    tone(buzzerPin, 500, 200);  // 500 Hz taajuus 500 millisekunnin ajan
+    delay(300);
+    tone(buzzerPin, 500, 500);  // 500 Hz taajuus 500 millisekunnin ajan
+    delay(300);
+    tone(buzzerPin, 500, 500);  // 500 Hz taajuus 500 millisekunnin ajan
   } 
   else if (body == "Logged out") {
     lcd.setRGB(255, 255, 255);  // Valkoinen väri (Logged out)
     lcd.setCursor(0, 0);
     lcd.print("Logged out");
+    tone(buzzerPin, 400, 300);  // 400 Hz taajuus 300 millisekunnin ajan
   }
 } 
 
